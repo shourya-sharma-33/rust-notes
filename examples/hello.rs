58f201e4c87d7cb534ef4d6de400c9bccbb01aae
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-08-27 14:21:41 +0530

    first com

---
+++ examples/hello.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("meow :3")
+}
+
+// to run u need to run `cargo run --example {file name}`
+
+// PS C:\Users\Dell\Downloads\hello> cargo run --example hello
+// >>
+//    Compiling hello v0.1.0 (C:\Users\Dell\Downloads\hello)
+//     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.56s
+//      Running `target\debug\examples\hello.exe`
+// meow :3
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("the main file");
+}
+
+// to run this code
+// run `cargo run` in terminal
